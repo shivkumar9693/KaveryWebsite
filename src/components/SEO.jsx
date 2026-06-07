2afce8c4e49efa347448b8bf35c694a0b899e668
shivkumar9693 <shivkumarthakur9693@gmail.com>
2026-06-07 23:06:49 +0530

    changes all section

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -42,7 +42,7 @@ const SEO = ({
     'name': 'Kavery Tubing Solutions Pvt. Ltd.',
     'alternateName': 'Kavery Tubing Solutions',
     'url': domain,
-    'logo': `${domain}/assets/logo_rounded.png`,
+    'logo': `${domain}/assets/logo.png`,
     'image': `${domain}/assets/hero_stainless_steel.png`,
     'description': defaultDescription,
     'telephone': '+91 98987 40495',
@@ -141,8 +141,8 @@ const SEO = ({
       <meta name="twitter:image" content={ogImage} />
 
       {/* Favicon & Web App Styling */}
-      <link rel="icon" type="image/png" href="/assets/logo_rounded.png" />
-      <link rel="apple-touch-icon" href="/assets/logo_rounded.png" />
+      <link rel="icon" type="image/png" href="/assets/logo.png" />
+      <link rel="apple-touch-icon" href="/assets/logo.png" />
       <meta name="theme-color" content="#D10000" />
 
       {/* Google Result Customization / Logo Markup */}
